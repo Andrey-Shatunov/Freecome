55e431fefdef1227f189f998c10ceb387393b269
Tapac174 <andreshatuno@gmail.com>
2017-01-30 09:23:10 +0500

    test

--- Новая папка/income_to.py
+++
@@ -1,8 +0,0 @@
-def entity_to_str(entity):
-    str1=""
-    for i,value in enumerate(entity,1):
-        str1=str1+"\'"+str(i)+"\'"+''': '''+str(value)+''', '''
-
-    str1=str1[0:(len(str1)-2)]
-    str1='''{'''+str1+'''}'''
-    return str1
